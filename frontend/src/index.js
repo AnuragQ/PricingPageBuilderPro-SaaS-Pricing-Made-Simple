c75a9d3fa27de37da66ba84defaf8f5472d25eb5
Akshat Nehra <akshatnehra1999@gmail.com>
2024-02-27 22:44:26 -0500

    feat: Light/Dark Mode toggle added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// Light/Dark mode
+import {createNightowl} from '@bufferhead/nightowl'
+  
+createNightowl({
+    defaultMode: 'dark',
+    toggleButtonMode: 'newState'
+})
+// End light/dark mode
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
