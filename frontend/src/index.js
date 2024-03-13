365593d8c933f793cc08e715434b6d5fc0c8d044
Akshat Nehra <akshatnehra1999@gmail.com>
2024-03-13 14:08:04 -0400

    feat: react query added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,19 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 // Light/Dark mode
-import {createNightowl} from '@bufferhead/nightowl'
+// import {createNightowl} from '@bufferhead/nightowl'
   
-createNightowl({
-    defaultMode: 'dark',
-    toggleButtonMode: 'newState'
-})
+// createNightowl({
+//     defaultMode: 'dark',
+//     toggleButtonMode: 'newState'
+// })
 // End light/dark mode
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
